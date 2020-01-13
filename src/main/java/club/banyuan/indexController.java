cd296c9454640de2da469639672b53f7c7c63931
0501a <13949693939zy>
2020-01-13 13:11:15 +0800

    0

--- src/main/java/club/banyuan/indexController.java
+++ src/main/java/club/banyuan/indexController.java
@@ -1,8 +1,15 @@
 package club.banyuan;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class indexController {
-    
+    @GetMapping("/users/{username:[a-z0-9_]+}")
+    @ResponseBody
+    public String getIndex(@PathVariable String username){
+        return String.format("user %s",username);
+    }
 }
